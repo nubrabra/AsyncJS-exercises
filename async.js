c2339054a87bd181075154fb5ab011a5aa8990ed
Panurut <nubrabra5@gmail.com>
2017-11-21 11:08:56 +0700

    'readfile'

--- async.js
+++ async.js
@@ -1,9 +1,23 @@
 const fs = require('fs');
 
-const readFromFile = (fileName)=>{
-  // WRITE CODE HERE
-};
+// const readFromFile = (fileName)=>{
+//   // WRITE CODE HERE
+//   return fs.readFileSync(fileName,'utf8');
+// };
 
-console.log('START');
-const data = readFromFile('sample.txt');
-console.log('END')
+const readFromFile = (pathName,callback) => {
+    return fs.readFile(pathName,'utf8',(err,data) => {
+        if (err) {
+            callback(err);
+        }
+        else {
+            callback(data);
+        }
+    });
+}
+
+readFromFile('sample.txt',(data) => {
+    console.log('START');
+    console.log(data);
+    console.log('END');
+});
